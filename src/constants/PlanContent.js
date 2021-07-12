c690cd04d4b167c2fd68383707abaed5185ea468
Deepunyk <deepaknayak.work@gmail.com>
2021-07-12 21:06:37 +0530

    plans and logout

--- src/constants/PlanContent.js
+++ src/constants/PlanContent.js
@@ -1,76 +1,93 @@
-const PlanContent = 
+const PlanContent = {
+  monthly: [
     {
-        "monthly":[
-            {
-                "title":"Silver Plan",
-                "line1":"DSE (Sales Man) - 1",
-                "line2": "Retailers - Unlimited",
-                "line3" : "Product/Operator",
-                "line4":"Unlimited",
-                "cost":"₹49 only",
-                "validity" :"Valid 28 days"
-            },
-            {
-                "title":"Gold Plan",
-                "line1":"DSE (Sales Man) - 4",
-                "line2": "Retailers - Unlimited",
-                "line3" : "Product/Operator",
-                "line4":"Unlimited",
-                "cost":"₹149 only",
-                "validity" :"Valid 28 days"
-            },
-            {
-                "title":"Diamond Plan",
-                "line1":"DSE (Sales Man) - 6",
-                "line2": "Retailers - Unlimited",
-                "line3" : "Product/Operator",
-                "line4":"Unlimited",
-                "cost":"₹199 only",
-                "validity" :"Valid 28 days"
-            },{
-                "title":"Platinum Plan",
-                "line1":"DSE Unlimited",
-                "line2": "Retailers - Unlimited",
-                "line3" : "Product/Operator",
-                "line4":"Unlimited",
-                "cost":"₹299 only",
-                "validity" :"Valid 28 days"
-            }
-        ],
+      title: "Silver Plan",
+      line1: "DSE (Sales Man) - 1",
+      line2: "Retailers - Unlimited",
+      line3: "Product/Operator",
+      line4: "Unlimited",
+      cost: "₹49 only",
+      validity: "Valid 28 days",
+      planId: 1000000001,
+      planType: "month",
+    },
+    {
+      title: "Gold Plan",
+      line1: "DSE (Sales Man) - 4",
+      line2: "Retailers - Unlimited",
+      line3: "Product/Operator",
+      line4: "Unlimited",
+      cost: "₹149 only",
+      validity: "Valid 28 days",
+      planId: 1000000002,
+      planType: "month",
+    },
+    {
+      title: "Diamond Plan",
+      line1: "DSE (Sales Man) - 6",
+      line2: "Retailers - Unlimited",
+      line3: "Product/Operator",
+      line4: "Unlimited",
+      cost: "₹199 only",
+      validity: "Valid 28 days",
+      planId: 1000000003,
+      planType: "month",
+    },
+    {
+      title: "Platinum Plan",
+      line1: "DSE Unlimited",
+      line2: "Retailers - Unlimited",
+      line3: "Product/Operator",
+      line4: "Unlimited",
+      cost: "₹299 only",
+      validity: "Valid 28 days",
+      planId: 1000000004,
+      planType: "month",
+    },
+  ],
 
-        "yearly":[
-            {
-                "title":"Silver Plan",
-                "line1":"DSE (Sales Man) - 1",
-                "line2": "Retailers - Unlimited",
-                "line3" : "Product/Operator",
-                "line4":"Unlimited",
-                "cost":"₹499 only",
-            },
-            {
-                "title":"Gold Plan",
-                "line1":"DSE (Sales Man) - 4",
-                "line2": "Retailers - Unlimited",
-                "line3" : "Product/Operator",
-                "line4":"Unlimited",
-                "cost":"₹1499 only",
-            },
-            {
-                "title":"Diamond Plan",
-                "line1":"DSE (Sales Man) - 6",
-                "line2": "Retailers - Unlimited",
-                "line3" : "Product/Operator",
-                "line4":"Unlimited",
-                "cost":"₹1999 only",
-            },{
-                "title":"Platinum Plan",
-                "line1":"DSE Unlimited",
-                "line2": "Retailers - Unlimited",
-                "line3" : "Product/Operator",
-                "line4":"Unlimited",
-                "cost":"₹2999 only",
-            }
-        ]
-    }
+  yearly: [
+    {
+      title: "Silver Plan",
+      line1: "DSE (Sales Man) - 1",
+      line2: "Retailers - Unlimited",
+      line3: "Product/Operator",
+      line4: "Unlimited",
+      cost: "₹499 only",
+      planId: 1000000001,
+      planType: "year",
+    },
+    {
+      title: "Gold Plan",
+      line1: "DSE (Sales Man) - 4",
+      line2: "Retailers - Unlimited",
+      line3: "Product/Operator",
+      line4: "Unlimited",
+      cost: "₹1499 only",
+      planId: 1000000002,
+      planType: "year",
+    },
+    {
+      title: "Diamond Plan",
+      line1: "DSE (Sales Man) - 6",
+      line2: "Retailers - Unlimited",
+      line3: "Product/Operator",
+      line4: "Unlimited",
+      cost: "₹1999 only",
+      planId: 1000000003,
+      planType: "year",
+    },
+    {
+      title: "Platinum Plan",
+      line1: "DSE Unlimited",
+      line2: "Retailers - Unlimited",
+      line3: "Product/Operator",
+      line4: "Unlimited",
+      cost: "₹2999 only",
+      planId: 1000000004,
+      planType: "year",
+    },
+  ],
+};
 
 export default PlanContent;
--- src/pages/plans/PlanCard.js
+++ src/pages/plans/PlanCard.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import color from '../../constants/color';
-import * as S from './PlansStyled';
+import React from "react";
+import color from "../../constants/color";
+import * as S from "./PlansStyled";
+import API from "../../utils/Api";
+import { useHistory } from "react-router-dom";
 
-const PlanCard= (props)=>{
+const PlanCard = (props) => {
+  const data = props.data;
+  let history = useHistory();
 
-const data = props.data;
+  const buyPlan = async () => {
+    await API.put("company/UpgradePlan", { planid: data.planId, plantype: data.planType });
+    history.push("/home");
+  };
 
-    return(
-        <S.CardWrapper>
-            <S.Text color={color.primary} weight='500' size='1.4rem'>{data.title}</S.Text>
-            <S.Text>{data.line1}</S.Text>
-            <S.Text>{data.line2}</S.Text>
-            <S.Text weight='500' line>{data.line3}</S.Text>
-            <S.Text>{data.line4}</S.Text>
-            <S.PriceText>{data.cost}</S.PriceText>
-            {data.validity &&      
-            <S.Text weight='500'>{data.validity}</S.Text>
-            }
-            <S.Button>BUY NOW</S.Button>
-        </S.CardWrapper>
-    );
-}
+  return (
+    <S.CardWrapper>
+      <S.Text color={color.primary} weight="500" size="1.4rem">
+        {data.title}
+      </S.Text>
+      <S.Text>{data.line1}</S.Text>
+      <S.Text>{data.line2}</S.Text>
+      <S.Text weight="500" line>
+        {data.line3}
+      </S.Text>
+      <S.Text>{data.line4}</S.Text>
+      <S.PriceText>{data.cost}</S.PriceText>
+      {data.validity && <S.Text weight="500">{data.validity}</S.Text>}
+      <S.Button onClick={() => buyPlan()}>BUY NOW</S.Button>
+    </S.CardWrapper>
+  );
+};
 
 export default PlanCard;
